4728c3bc174e30832aee1b80a581534999885f5f
Administrator <Administrator@YT-pc>
2018-12-08 13:55:10 +0800

    HelloJava

---
+++ 课件项目/src/第一章/代码/HelloJava.java
@@ -0,0 +1,19 @@
+package 第一章.代码;
+/**
+ * 文档注释
+ * @author ivey
+ * @version 1.0
+ */
+public class HelloJava {
+
+	/*
+	 * 这个是多行注释
+	 * 这个方法是java程序的入口方法
+	 */
+	public static void main(String[] args) {
+		//在控制台输出一句问候语
+		System.out.println("Hello Java!您好！");
+		System.out.println("Java开发挺爽的！");
+	}
+
+}
